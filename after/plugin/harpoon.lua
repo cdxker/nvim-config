9f857f064c1132c17d7fd43ac1c7cfaae4f33f46
Denzell Ford <fdenzell@gmail.com>
2023-01-05 17:40:02 -0600

    Changed it to <S-m>

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -4,7 +4,7 @@ local ui = require("harpoon.ui")
 
 
 vim.keymap.set("n", "<leader>m", mark.add_file)
-vim.keymap.set("n", "<C-m>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<S-m>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<leader>ta", function() term.gotoTerminal(1) end)
 vim.keymap.set("n", "<leader>ts", function() term.gotoTerminal(2) end)
@@ -15,5 +15,3 @@ vim.keymap.set("n", "<leader>ma", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<leader>ms", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<leader>md", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<leader>mf", function() ui.nav_file(4) end)
-
-
