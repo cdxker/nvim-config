eaf92a7ab15b345b4cf0923bd300b93ec736ced3
fastfist <fdenzell@gmail.com>
2024-02-08 20:08:24 -0800

    feature: removed random lldb dependency

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -85,12 +85,12 @@ vim.diagnostic.config({
 })
 
 local rt = require("rust-tools")
-local mason_registry = require("mason-registry")
-
-local codelldb = mason_registry.get_package("codelldb")
-local extension_path = codelldb:get_install_path() .. "/extension/"
-local codelldb_path = extension_path .. "adapter/codelldb"
-local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
+-- local mason_registry = require("mason-registry")
+--
+-- local codelldb = mason_registry.get_package("codelldb")
+-- local extension_path = codelldb:get_install_path() .. "/extension/"
+-- local codelldb_path = extension_path .. "adapter/codelldb"
+-- local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
 
 
 local rust_lsp = lsp.build_options('rust_analyzer', {
